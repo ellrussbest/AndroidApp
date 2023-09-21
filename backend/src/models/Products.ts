7c59a0fcaa459b93f27eefbc384e7c63c6228426
ellrussbest <ellrussbest@gmail.com>
2023-09-21 14:11:54 +0200

    message

--- backend/src/models/Products.ts
+++ backend/src/models/Products.ts
@@ -2,6 +2,13 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const productsSchema = new Schema({});
+const productsSchema = new Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
 
 export const Products = mongoose.model("Products", productsSchema);
---
+++ backend/src/routes/orderItems-routes.ts
@@ -0,0 +1,3 @@
+import express from "express";
+
+export const orderItemsRouter = express.Router();
